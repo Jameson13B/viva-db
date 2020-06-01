8763e902d787f03cf366ccc3b55da7f5ca2838ae
Jameson.Brown <jameson.brown@mx.com>
2020-06-01 16:42:29 -0600

    Created getProgress util for percentage complete calc

--- src/utils.js
+++ src/utils.js
@@ -2,3 +2,5 @@
 const d = new Date()
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 export const getTodaysDay = () => days[d.getDay()]
+
+export const getProgress = (count, max) => (count / max) * 100
